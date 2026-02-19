2c22e4b46b6f1adb09e687b3f61706abc0b6f9c8
Felix Gao <llai92641@gmail.com>
2026-02-18 23:13:52 -0500

    finished utils.py

---
+++ backend/src/utils.py
@@ -0,0 +1,27 @@
+from fastapi import HTTPException
+from clerk_backend_api import Clerk, AuthenticateRequestOptions
+import os
+from dotenv import load_dotenv
+
+# looking for envrionment file and environemt variables, giving us access to it
+load_dotenv()
+
+clerk_sdk = Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY"))
+
+def authenticate_and_get_user_details(request):
+    try:
+        request_state = clerk_sdk.authenticate_request(
+            request,
+            AuthenticateRequestOptions(
+                authorized_parties=["http://localhost:5173", "http://localhost:5174"],
+                jwt_key=os.getenv("JWT_KEY") #makes it happen serverless, if its commented out then it actualyl sends the request and makes it slower
+            )
+        )
+        if not request_state.is_signed_in:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        
+        user_id = request_state.payload.get("sub")
+        
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
