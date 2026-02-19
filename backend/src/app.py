66f1f0172db0f95ecca001867a769ab29103f84e
Felix Gao <llai92641@gmail.com>
2026-02-18 22:58:52 -0500

    added uvicorn

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,3 +4,10 @@ from clerk_backend_api import Clerk
 import os
 
 clerk_sdk = Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY"))
+
+app = FastAPI()
+
+# set this to the local port or something else to make it more secure
+# uses * rn to avoid errors
+# could change it to localhost5173
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
